c4040b08796ec2cd1e88ebcfd793ec5704147668
Yassin Hammami <yas.hammami@gmail.com>
2022-12-10 13:31:07 +0100

    Je viens de créer les liste de pokemons

--- ng-pokedex-app/src/app/app.component.ts
+++ ng-pokedex-app/src/app/app.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { pokemonDetail } from './pokemon-detail';
+import { pokemonL } from './pokemon-list';
 
 @Component({
   selector: 'app-root',
   template: `
       <h1>
-        Welcome to {{pokemonList[0]}}!
+        Voici une liste de {{pokemonList}}!
       </h1>
       `
 })
+
 export class AppComponent implements OnInit {
   title = 'ng-pokedex-app';
-  pokemonList = ['Salameche','Bulbizarre','Carapuce']
+  pokemonList: pokemonDetail[] = pokemonL ; 
+
+
+
 
   ngOnInit() {
-    console.table(this.pokemonList)
-    this.selectPokemon('Bulbizarre')
+    console.table(pokemonL)
+    this.selectPokemon(this.pokemonList[0])
+
   }
-  selectPokemon(pokmeonName: string) {
-    console.log(`Vous avez cliquez sur le pokemon ${pokmeonName}`);
+  selectPokemon(pokemonL : pokemonDetail) {
+    console.log(`Vous avez cliquez sur le pokemon ${pokemonDetail.name}`);
   }
 }
---
+++ ng-pokedex-app/src/app/pokemon-detail.ts
@@ -0,0 +1,10 @@
+
+export class pokemonDetail {
+    id!: number;
+    hp!: number;
+    cp!: number;
+    name!: string;
+    picture!: string;
+    types!: Array<string>;
+    created!: Date;
+  }
